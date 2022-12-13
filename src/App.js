5523b208cea107d849c5b962b70f39bae7f0a3a8
Pavel <paha_gagarin@mail.ru>
2022-12-13 14:47:22 +0300

    мобильное меню

--- src/App.js
+++ src/App.js
@@ -3,17 +3,21 @@ import React from 'react';
 import './App.scss';
 import { useState } from 'react';
 function App() {
-  const [burger, setBurger] = useState(false)
+  const [burgerAndMobileMenu, setBurgerAndMobileMenu] = useState(false)
+  const [tehemeToggle, setThemeToggle] = useState(false);
   const clickHandler = () => {
     console.log('click')
-    setBurger(!burger)
+    setBurgerAndMobileMenu(!burgerAndMobileMenu)
+  }
+  const themeToggleHandler = () => {
+    setThemeToggle(!tehemeToggle)
   }
   return (
     <div className="site-container">
       <header className='header'>
         <div className="container">
           <div className="nav-container">
-            <button className={`burger ${burger ? 'active' : null}`} onClick={clickHandler}>
+            <button className={`burger ${burgerAndMobileMenu ? 'active' : ''}`} onClick={clickHandler}>
               <span className='burger__line burger__line--top'></span>
               <span className='burger__line burger__line--middle'></span>
               <span className='burger__line burger__line--bottom'></span>
@@ -26,7 +30,7 @@ function App() {
                 <li className='nav__item'><a href="#" className="nav__link">Контакты</a></li>
               </ul>
             </nav>
-            <button className='theme-toogle'>
+            <button className={`theme-toogle ${tehemeToggle ? 'dark' : 'light'}`} onClick={themeToggleHandler}>
               <img className='theme-toogle__sun' src='/images/sun.svg' alt='Светлая тема'></img>
               <img className='theme-toogle__sun' src='/images/moon.svg' alt='Темная тема'></img>
             </button>
@@ -92,6 +96,19 @@ function App() {
           </ul>
         </div>
       </footer>
+      <div className={`mobile-menu ${burgerAndMobileMenu ? 'active' : ''}`}>
+        <nav className="nav">
+          <ul className='nav__list'>
+            <li className='nav__item'><a href="#" className="nav__link active">Мои работы</a></li>
+            <li className='nav__item'><a href="#" className="nav__link">Навыки</a></li>
+            <li className='nav__item'><a href="#" className="nav__link">Контакты</a></li>
+          </ul>
+        </nav>
+        <button className='burger-close' onClick={clickHandler}>
+          <span className='burger-close__line burger-close__line--top'></span>
+          <span className='burger-close__line burger-close__line--bottom'></span>
+        </button>
+      </div>
     </div>
   );
 }
