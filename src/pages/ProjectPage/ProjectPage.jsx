36eb7301f9f46d4a60a21aee1198338c69cff13a
Pavel <paha_gagarin@mail.ru>
2022-12-18 18:24:55 +0300

    верскат детальной страницы проета, доработка базы данных

--- src/pages/ProjectPage/ProjectPage.jsx
+++ src/pages/ProjectPage/ProjectPage.jsx
@@ -1,12 +1,54 @@
+import { logDOM } from "@testing-library/react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import "./ProjectPage.scss";
 
 const ProjectPage = () => {
+  const [currentProjects, setCurrentProjects] = useState([]);
   const { title } = useParams();
+  useEffect(() => {
+    fetch("../dataBase.json")
+      .then((res) => res.json())
+      .then((data) => {
+        setCurrentProjects(data.filter((item) => item.title === title));
+      });
+  }, []);
   return (
-    <div className="project">
+    <div className="current-project">
       <div className="container">
-        <h1>Project {title}</h1>
+        {currentProjects.map((currentProject) => {
+          return (
+            <div
+              key={currentProject.title}
+              className="current-project__wrapper"
+            >
+              <div className="current-project__left">
+                <img
+                  className="current-project__img"
+                  src={"../" + currentProject.img}
+                  alt={currentProject.title}
+                ></img>
+              </div>
+              <div className="current-project__right">
+                <h3 className="title current-project__title">
+                  {currentProject.title}
+                </h3>
+                <div className="current-project-stack">
+                  <p className="current-project-stack__title">
+                    Используемые технологии:
+                  </p>
+                  <p className="current-project-stack__description">
+                    {currentProject.stack}
+                  </p>
+                </div>
+                <div className="current-project__links">
+                  <a className="current-project__link" href={currentProject.urlVercel} target="_blank">Web-страничка проекта</a>
+                  <a className="current-project__link" href={currentProject.urlGithub} target="_blank">Репозиторий проекта</a>
+                </div>
+              </div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
