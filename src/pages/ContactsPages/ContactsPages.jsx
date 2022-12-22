f4d3e1708eab7797306a8a0cbc9a66728257580c
Pavel <paha_gagarin@mail.ru>
2022-12-22 15:10:02 +0300

    df

--- src/pages/ContactsPages/ContactsPages.jsx
+++ src/pages/ContactsPages/ContactsPages.jsx
@@ -7,7 +7,7 @@ const ContactsPages = () => {
         <h1 className='title contacts__title'>Контакты</h1>
         <h3 className='contacts__min-title'>Адресс</h3>
         <p className='contacts__description'>Смоленска область , город Гагарин</p>
-        <h1 className='title contacts__min-title'>Telegram/Whatsap</h1>
+        <h1 className='title contacts__min-title'>Telegram / Whatsap</h1>
         <a href="tel:+79621944028" className='contacts__link contacts__link--margin-bottom'>8-962-194-40-28</a>
         <h1 className='title contacts__min-title'>Email</h1>
         <a href="mailto:paha_gagarin@mail.ru" className='contacts__link'>paha_gagarin@mail.ru</a>
--- src/pages/SkillsPages/SkillsPages.jsx
+++ src/pages/SkillsPages/SkillsPages.jsx
@@ -5,7 +5,7 @@ const SkillsPages = () => {
     <div className='skills'>
       <div className="container">
       <h1 className='title skills__title'>FRONTEND</h1>
-      <p className='skills__description'>HTML,CSS,JS,REACT,GIT,PHOTOSHOPE,FIGMA</p>
+      <p className='skills__description'>HTML, CSS, JS, REACT, GULP, WEBPACK, GIT, PHOTOSHOPE, FIGMA</p>
       </div>
     </div>
   )
